ffe21562c0a362085dcbc47eb34dc8bec8d3c38b
Flaymed <mvigg8@gmail.com>
2020-04-03 09:39:30 -0400

    Initial Commit
    Main Class, just the enable and disable methods.

---
+++ src/main/java/me/flaymed/islands/Main.java
@@ -0,0 +1,18 @@
+package me.flaymed.islands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import com.podcrash.api.mc.game.GameManager;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}

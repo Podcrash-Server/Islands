41d43fa200e29dd10ac71fca0c0c61b9711e4c7f
Flaymed <mvigg8@gmail.com>
2020-04-09 23:11:55 -0400

    Bridges

--- src/main/java/me/flaymed/islands/game/map/bridge/BridgeManager.java
+++ src/main/java/me/flaymed/islands/game/map/bridge/BridgeManager.java
@@ -3,6 +3,7 @@ package me.flaymed.islands.game.map.bridge;
 import com.podcrash.api.db.pojos.map.IslandsMap;
 import me.raindance.chunk.WorldScanner;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 
@@ -26,30 +27,20 @@ public class BridgeManager {
 
         for (Map.Entry bridgePart : bridgeParts.entrySet()) {
 
-            LinkedList<Block> bridgeBlocks = new LinkedList<>();
-
             Location piece1 = (Location) bridgePart.getKey();
             Location piece2 = (Location) bridgePart.getValue();
 
-            double xDifference = piece1.getBlockX() - piece2.getBlockX();
-            double yDifference = piece1.getBlockY() - piece2.getBlockY();
-            double zDifference = piece1.getBlockZ() - piece2.getBlockZ();
-            //If piece1 X is smaller than piece2 X then the calculated difference will be less than 0, multiply it by negative one to make it positive
-            //Same for Y & Z values
-            xDifference = makePositive(xDifference);
-            yDifference = makePositive(yDifference);
-            zDifference = makePositive(zDifference);
+            //Y Difference not needed
+            double xDifference = Math.abs(piece1.getBlockX() - piece2.getBlockX());
+            double zDifference = Math.abs(piece1.getBlockZ() - piece2.getBlockZ());
 
-            double bridgeWidth;
+            //Only need width for direction
             double bridgeLength;
 
-            if (xDifference > zDifference) {
-                bridgeWidth = zDifference;
+            if (xDifference > zDifference)
                 bridgeLength = xDifference;
-            } else {
-                bridgeWidth = xDifference;
+            else
                 bridgeLength = zDifference;
-            }
 
             int highestX = Math.max(piece1.getBlockX(), piece2.getBlockX());
             int lowestX = Math.min(piece1.getBlockX(), piece2.getBlockX());
@@ -60,35 +51,45 @@ public class BridgeManager {
             int highestZ = Math.max(piece1.getBlockZ(), piece2.getBlockZ());
             int lowestZ = Math.min(piece1.getBlockZ(), piece2.getBlockZ());
 
-            for (int x = lowestX; x < highestX; x++) {
-                for (int y = lowestY; y < highestY; y++) {
-                    for (int z = lowestZ; z < highestZ; z++) {
-                        Block block = world.getBlockAt(x, y, z);
-                        bridgeBlocks.add(block);
+            LinkedList<Object> section = new LinkedList<>();
+            for (int i = 0; i < bridgeLength; i++) {
+
+                //Z is the width of the bridge
+                if (xDifference > zDifference) {
+
+                    int startingPos = highestX - 1;
+                    for (int x = startingPos; x > startingPos - bridgeLength; x--) {
+                        for (int y = lowestY; y < highestY; y++) {
+                            for (int z = lowestZ; z < highestZ; z++) {
+                                if (world.getBlockAt(x, y, z).getType().equals(Material.AIR))
+                                    continue;
+                                section.add(new Location(world, x, y, z));
+                                section.add(world.getBlockAt(x, y, z));
+                                map.addBridgePiece(i, section);
+                            }
+                        }
+                        section.clear();
+                    }
+
+                //X is the width of the bridge
+                } else {
+                    int startingPos = highestZ - 1;
+                    for (int z = startingPos; z > lowestZ; z--) {
+                        for (int y = lowestY; y < highestY; y++) {
+                            for (int x = lowestX; x < highestX; x++) {
+                                if (world.getBlockAt(x, y, z).getType().equals(Material.AIR))
+                                    continue;
+                                section.add(new Location(world, x, y, z));
+                                section.add(world.getBlockAt(x, y, z));
+                                map.addBridgePiece(i, section);
+                            }
+                        }
+                        section.clear();
                     }
                 }
             }
 
-            for (int i = 0; i < bridgeBlocks.size(); i++) {
-
-            }
-
-
-
-
         }
-    }
-
-    /**
-     * Provide a number, if the number is less than 0, the number will be set equal to itself times -1 to make it positive.
-     * @param number Number to make positive
-     * @return Returns positive form of the number or the number if it is already positive
-     */
-    public double makePositive(double number) {
-        if(number < 0)
-            number *= -1;
-
-        return number;
     }
 
 }

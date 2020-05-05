f590e81a80c06919782f92a5b2a9df1eaed286cf
Flaymed <mvigg8@gmail.com>
2020-05-05 10:21:30 -0400

    Takedown
    Needs some testing takedown should be pretty much done.

--- src/main/java/me/flaymed/islands/kits/skills/berserker/Leap.java
+++ src/main/java/me/flaymed/islands/kits/skills/berserker/Leap.java
@@ -1,21 +1,36 @@
 package me.flaymed.islands.kits.skills.berserker;
 
+import com.comphenix.protocol.wrappers.EnumWrappers;
+import com.packetwrapper.abstractpackets.WrapperPlayServerWorldParticles;
+import com.podcrash.api.callback.sources.CollideBeforeHitGround;
+import com.podcrash.api.damage.DamageApplier;
+import com.podcrash.api.effect.particle.ParticleGenerator;
 import com.podcrash.api.events.DamageApplyEvent;
+import com.podcrash.api.events.skill.SkillUseEvent;
+import com.podcrash.api.game.GameManager;
 import com.podcrash.api.kits.enums.ItemType;
 import com.podcrash.api.kits.iskilltypes.action.ICharge;
+import com.podcrash.api.kits.iskilltypes.action.IConstruct;
 import com.podcrash.api.kits.iskilltypes.action.ICooldown;
 import com.podcrash.api.kits.skilltypes.Instant;
+import com.podcrash.api.util.PacketUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.util.Vector;
+import java.util.List;
 
-public class Leap extends Instant implements ICooldown, ICharge {
+public class Leap extends Instant implements ICooldown, ICharge, IConstruct {
 
+    private final float hitbox = 0.45f;
+    private CollideBeforeHitGround hitGround;
     private final int MAX_CHARGES = 8;
     private int charges = MAX_CHARGES;
     private int hits = 0;
@@ -114,4 +129,32 @@ public class Leap extends Instant implements ICooldown, ICharge {
         }
     }
 
+    @Override
+    public void afterConstruction() {
+        WrapperPlayServerWorldParticles packet = ParticleGenerator.createParticle(EnumWrappers.Particle.CRIT, 2);
+        this.hitGround = new CollideBeforeHitGround(getPlayer(), 1, hitbox, hitbox, hitbox)
+                .then(() -> {
+                    SkillUseEvent event = new SkillUseEvent(this);
+                    Bukkit.getPluginManager().callEvent(event);
+                    if(event.isCancelled()) return;
+                    List<Entity> entities = CollideBeforeHitGround.getValidEntitiesInRange(getPlayer(), hitbox, hitbox, hitbox);
+                    if (entities.size() == 0) return;
+                    getPlayer().setVelocity(new Vector(0, 0, 0));
+                    for (Entity entity : entities) {
+                        if (entity instanceof LivingEntity && entity != getPlayer() && !isAlly((LivingEntity) entity)) {
+                            if (entity instanceof Player && GameManager.isSpectating((Player) entity)) break;
+                            LivingEntity living = (LivingEntity) entity;
+                            getPlayer().getWorld().playSound(getPlayer().getLocation(), Sound.ZOMBIE_WOOD, 2f, 0.2f);
+                            DamageApplier.damage(living, getPlayer(), 8, this, false);
+
+                            getPlayer().sendMessage(getUsedMessage(living));
+
+                            break; //only attack one player
+                        }
+                    }
+                }).doWhile(() -> {
+                    packet.setLocation(getPlayer().getLocation());
+                    PacketUtil.asyncSend(packet, getPlayers());
+                });
+    }
 }

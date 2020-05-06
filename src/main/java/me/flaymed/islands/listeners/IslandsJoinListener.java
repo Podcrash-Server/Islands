ca3a61f0caaeab1789d68232c072724d39680fbe
rain474 <lordraindance5@gmail.com>
2020-05-06 15:29:21 -0400

    ore gen basic stuF

--- src/main/java/me/flaymed/islands/listeners/IslandsJoinListener.java
+++ src/main/java/me/flaymed/islands/listeners/IslandsJoinListener.java
@@ -4,12 +4,19 @@ import com.podcrash.api.game.GameManager;
 import com.podcrash.api.game.GameState;
 import com.podcrash.api.kits.KitPlayerManager;
 import com.podcrash.api.listeners.ListenerBase;
+import com.podcrash.api.plugin.PodcrashSpigot;
 import me.flaymed.islands.kits.classes.Berserker;
 import me.flaymed.islands.kits.classes.Medic;
+import me.flaymed.islands.util.ore.VeinBuilder;
+import me.flaymed.islands.util.ore.VeinGen;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -38,5 +45,26 @@ public class IslandsJoinListener extends ListenerBase {
                 GameManager.addPlayer(player);
             }
         }
+    }
+
+    @EventHandler
+    public void interactTest(PlayerInteractEvent e) {
+        Player player = e.getPlayer();
+        ItemStack item = player.getItemInHand();
+        Material ore = item.getType();
+        Action action = e.getAction();
+        if (action != Action.RIGHT_CLICK_BLOCK)
+            return;
+        VeinBuilder builder = new VeinBuilder()
+            .setOreChance(0.4)
+            .setContinueChance(0.6)
+            .setTries(15);
+        VeinGen generator = VeinGen.fromBuilder(builder);
+        generator.startLocation(e.getClickedBlock().getLocation(), ore);
+        int max = 6;
+        while (generator.getOreGenerated() < max && generator.hasNext())
+            generator.next();
+
+        PodcrashSpigot.debugLog("Generated " + generator.getOreGenerated() + " of " + ore);
     }
 }

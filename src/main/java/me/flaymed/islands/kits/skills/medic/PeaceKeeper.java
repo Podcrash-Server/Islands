f90d46e1d16c68cc1f1a8d6862ebfa4e7cad5e19
Flaymed <mvigg8@gmail.com>
2020-05-06 13:21:41 -0400

    Fixed Passive

--- src/main/java/me/flaymed/islands/kits/skills/medic/PeaceKeeper.java
+++ src/main/java/me/flaymed/islands/kits/skills/medic/PeaceKeeper.java
@@ -9,7 +9,9 @@ import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -19,6 +21,7 @@ public class PeaceKeeper extends Passive implements IConstruct {
     private Material leggings = Material.DIAMOND_LEGGINGS;
     private Material sword = Material.DIAMOND_SWORD;
     private String kit = "Medic";
+    private String noUseMessage = String.format("%s Skill> %sYou cannot use this item as a %s!", ChatColor.BLUE, ChatColor.GRAY, kit);
 
     public PeaceKeeper() {
 
@@ -42,7 +45,7 @@ public class PeaceKeeper extends Passive implements IConstruct {
 
             if (e.getCurrentItem().getType().equals(chestplate) || e.getCurrentItem().getType().equals(leggings) || e.getCurrentItem().getType().equals(sword)) {
                 e.setCancelled(true);
-                player.sendMessage(String.format("%s Skill> %sYou cannot use this item as a %s!", ChatColor.BLUE, ChatColor.GRAY, kit));
+                player.sendMessage(noUseMessage);
             }
 
         }
@@ -57,7 +60,24 @@ public class PeaceKeeper extends Passive implements IConstruct {
         if (player == getPlayer()) {
             if (item.getType().equals(chestplate) || item.getType().equals(leggings) || item.getType().equals(sword)) {
                 e.setCancelled(true);
-                player.sendMessage(String.format("%s Skill> %sYou cannot use this item as a %s!", ChatColor.BLUE, ChatColor.GRAY, kit));
+                player.sendMessage(noUseMessage);
+            }
+        }
+
+    }
+
+    @EventHandler
+    public void playerClick(PlayerInteractEvent e) {
+        Player player = e.getPlayer();
+        Action action = e.getAction();
+        Material holding = player.getItemInHand().getType();
+
+        if (player == getPlayer()) {
+            if (action.equals(Action.LEFT_CLICK_AIR) || action.equals(Action.LEFT_CLICK_BLOCK)) {
+                if (holding.equals(chestplate) || holding.equals(leggings) || holding.equals(sword)) {
+                    e.setCancelled(true);
+                    player.sendMessage(noUseMessage);
+                }
             }
         }
 

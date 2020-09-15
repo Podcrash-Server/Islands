b3fc8f0e0d59c38f15003e13611e21a97285e522
Flaymed <mvigg8@gmail.com>
2020-09-14 20:23:06 -0400

    Base Kits

--- src/main/java/me/flaymed/islands/inventory/KitSelectGUI.java
+++ src/main/java/me/flaymed/islands/inventory/KitSelectGUI.java
@@ -1,11 +1,39 @@
 package me.flaymed.islands.inventory;
 
+import me.flaymed.islands.kits.IslandsPlayer;
+import org.bukkit.inventory.Inventory;
+import java.util.ArrayList;
+import java.util.List;
+
 public class KitSelectGUI {
 
     private static KitSelectGUI INSTANCE;
+    private List<IslandsPlayer> baseKits;
+    private Inventory kitSelectInventory;
 
-    public KitSelectGUI() {
+    public KitSelectGUI(Class<? extends IslandsPlayer>... baseKits) {
         INSTANCE = this;
+
+        createBaseKits(baseKits);
+        createInventory();
+    }
+
+    private void createBaseKits(Class<? extends IslandsPlayer>... kits) {
+        List<IslandsPlayer> kitList = new ArrayList<>();
+        for (Class<? extends IslandsPlayer> kit : kits) {
+            try {
+                IslandsPlayer baseKit = kit.newInstance();
+                kitList.add(baseKit);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        this.baseKits = kitList;
+    }
+
+    private void createInventory() {
+
     }
 
     public static KitSelectGUI getInstance() {

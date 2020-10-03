698dfa33f59e986d18e8de4c66ce8bf76c255a21
Flaymed <mvigg8@gmail.com>
2020-10-03 17:31:09 -0400

    Refactor IslandsPlayer from GameEngine to GameCore

--- src/main/java/me/flaymed/islands/kits/IslandsPlayer.java
+++ src/main/java/me/flaymed/islands/kits/IslandsPlayer.java
@@ -1,44 +1,13 @@
 package me.flaymed.islands.kits;
 
-import com.google.gson.JsonObject;
-import com.podcrash.api.kits.KitPlayer;
-import com.podcrash.api.kits.Skill;
+import com.podcrash.gamecore.kits.KitPlayer;
 import org.bukkit.entity.Player;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class IslandsPlayer extends KitPlayer {
-    private final String name;
 
-    public IslandsPlayer(Player player, String name, Class<? extends Skill>... skills) {
+    public IslandsPlayer(Player player) {
         super(player);
-        this.name = name;
-
-        setUpSkills(skills);
     }
 
-    private void setUpSkills(Class<? extends Skill>... skills) {
-        Set<Skill> skillsSet = new HashSet<>();
-        for (Class<? extends Skill> skillClass : skills) {
-            try {
-                Skill skill = skillClass.newInstance();
-                skillsSet.add(skill);
-            } catch (InstantiationException | IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
 
-        this.skills = skillsSet;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public JsonObject serialize() {
-        return new JsonObject();
-    }
 }

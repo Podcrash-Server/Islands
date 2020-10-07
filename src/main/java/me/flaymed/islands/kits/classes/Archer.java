432c351effa751cdac1d10afd7d36e957a9bfc81
Flaymed <mvigg8@gmail.com>
2020-10-07 07:48:46 -0400

    Finished Archer

--- src/main/java/me/flaymed/islands/kits/classes/Archer.java
+++ src/main/java/me/flaymed/islands/kits/classes/Archer.java
@@ -1,15 +1,17 @@
 package me.flaymed.islands.kits.classes;
 
-import com.podcrash.gamecore.kits.Ability;
 import com.podcrash.gamecore.kits.Kit;
+import me.flaymed.islands.kits.skills.archer.Barrage;
+import me.flaymed.islands.kits.skills.archer.SkillSelect;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import java.util.List;
 
 public class Archer extends Kit {
 
-    public Archer(Class<? extends Ability>... abilities) {
-        super("Archer", abilities);
+    public Archer() {
+        //Only add SkillSelect and Barrage because other abilities are added through SkillSelect.
+        super("Archer", Barrage.class, SkillSelect.class);
     }
 
     @Override
